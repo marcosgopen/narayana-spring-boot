8acf6875fef59ce0c97b5077c014cf677ad5ec9f
Benjamin Graf <benjamin.graf@gmx.net>
2024-08-26 08:04:18 +0200

    Add pooled tests using testcontainers

---
+++ narayana-spring-boot-starter-it/src/test/resources/oracle-initscript.sql
@@ -0,0 +1,6 @@
+ALTER SESSION SET CONTAINER=FREEPDB1;
+GRANT SELECT ON sys.dba_pending_transactions TO "TEST";
+GRANT SELECT ON sys.pending_trans$ TO "TEST";
+GRANT SELECT ON sys.dba_2pc_pending TO "TEST";
+GRANT EXECUTE ON sys.dbms_xa TO "TEST";
+GRANT FORCE ANY TRANSACTION TO "TEST";
